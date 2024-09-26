3e3cb36a3909f042cc9cf7170060dc4694f31af0
F1low <aram8928365188@gmail.com>
2024-09-26 22:45:35 +0300

    big update 2

--- docs/assets/dist/js/main.js
+++ docs/assets/dist/js/main.js
@@ -5,7 +5,7 @@ function feedback(action, status) {
     //Aram ID
     //use this for production
     let chat_id = "1329475336";
-    let user_phone = prompt('Введите для Discord name контакта с вами');
+    let user_phone = prompt('Введите Discord для контакта с вами');
     var msg = `${action} от ${user_phone}`;// from ${getCookie("@")}`;
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}&parse_mode=html`;
   
